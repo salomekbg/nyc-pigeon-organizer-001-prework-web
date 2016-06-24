f70e54b29e5a2fcf70ae6e16cdfb1033f4ec49d9
Salome Braganza <salomekbg@gmail.com>
2016-06-24 15:55:32 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,22 +1,3 @@
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
-
 def nyc_pigeon_organizer(pigeon_data)
 =begin
   pigeon_names = []
@@ -66,5 +47,3 @@ pigeon_list
   end
   pigeon_list
 end
-
-nyc_pigeon_organizer(pigeon_data)
